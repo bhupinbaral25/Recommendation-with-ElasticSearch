77c111e645d82d804f425f1c6c328f968cbd77fe
bhupin <bhupin@bottle.com.np>
2022-12-08 12:38:47 +0545

    file to initilize index in Elastic search

---
+++ src/model_index.py
@@ -0,0 +1,36 @@
+from utils import cfg, ES
+from utils import error_handler
+
+@error_handler
+def create_index():
+    """
+    Create the index in elastic search with the mapping.
+    """
+    model_body = {
+        "settings": {
+            "number_of_shards": 2,
+            "number_of_replicas": 1,
+        },
+        "mappings": {
+            "properties": {
+                "movie_id": {"type": "text"},
+                "movie_name": {
+                    "type": "text",
+                },
+                "embedding": {"type": "dense_vector", "dims": 384},
+                "genra": {
+                    "type": "text",
+                }
+            }
+        },
+    }
+    ES.indices.create(index=cfg['index'], body=model_body)
+
+def insert_in_elastic(data):
+    """
+    Insert the data in the elastic search.
+    """
+    if not ES.indices.exists(index=cfg['index']):
+        create_index()
+    ES.index(index=cfg['index'], body=data)
+
